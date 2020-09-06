2be159c20812ae0a2c8d7d696fb6a323e569c558
Sebastian <sebastian.holler@tum.de>
2020-09-06 17:12:33 +0200

    added interface for character movement textfile input

---
+++ Unity/Assets/HumanMotionGeneration/CharacterMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    string[] actions;
+    string[] parameter;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("Please enter the path to the text file with actions:");
+        string path = Console.ReadLine();
+        string[] lines = ReadFile.ReadLines(path);
+        actions = new string[lines.Length];
+        parameter = new string[lines.Length];
+        for(int i=0; i<lines.Length; i++)
+        {
+            string line = lines[i];
+            string[] split = line.Split(':');
+            actions[i] = split[0];
+            parameter[i] = split[1];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string action = actions[counter];
+        string param = parameter[counter];
+        Character character = gameObject.AddComponent<Character>();
+        switch (action)
+        {
+            case "wait":
+                System.Threading.Thread.Sleep(Int32.Parse(param));
+                break;
+            case "move":
+                string[] split = param.Split(',');
+                character.NavGoTo(new Vector3(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2])));
+                break;
+            case "leftHand":
+            case "rightHand":
+                split = param.Split(',');
+                character.ReachFor(new Vector3(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2])));
+                break;
+            default:
+                break;
+        }
+        counter++;
+    }
+}
---
+++ Unity/Assets/HumanMotionGeneration/ReadFile.cs
@@ -0,0 +1,7 @@
+﻿public class ReadFile
+{
+    public static string[] ReadLines(string path)
+    {
+        return System.IO.File.ReadAllLines(path);
+    }
+}
