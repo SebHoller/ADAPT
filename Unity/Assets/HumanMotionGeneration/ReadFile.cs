3ca679d0fdaaeaa857d238e566fb4dc0f5a70457
Sebastian <sebastian.holler@tum.de>
2020-09-09 17:19:20 +0200

    added license

--- Unity/Assets/HumanMotionGeneration/ReadFile.cs
+++ Unity/Assets/HumanMotionGeneration/ReadFile.cs
@@ -1,4 +1,27 @@
-﻿public class ReadFile
+﻿#region License
+/*
+* HumanMotionGeneration an expansion of Agent Development and Prototyping Testbed
+* https://github.com/Sebauer98/ADAPT
+* 
+* Copyright (C) 2020 Sebastian Holler
+*
+* This file is part of ADAPT.
+* 
+* ADAPT is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ADAPT is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with ADAPT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+public class ReadFile
 {
     public static string[] ReadLines(string path)
     {
