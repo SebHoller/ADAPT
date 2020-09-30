5c1ab23d1a6dfbf6f6a52a7d755a64f6dee814a7
Sebastian <sebastian.holler@tum.de>
2020-09-30 14:49:49 +0200

    Added the possibility to add objects into the scene, loaded from stl-files

---
+++ Unity/Assets/HumanMotionGeneration/AddObject.cs
@@ -0,0 +1,68 @@
+﻿#region License
+/*
+* HumanMotionGeneration an expansion of Agent Development and Prototyping Testbed
+* https://github.com/Sebauer98/ADAPT
+* 
+* Copyright (C) 2020 Sebastian Holler
+*
+* This file is part of HumanMotionGeneration.
+* This project extends ADAPT.
+* 
+* ADAPT is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ADAPT is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with ADAPT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+using System;
+using UnityEngine;
+
+public class AddObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Console.WriteLine("Please enter the path to your stl-file");
+        string path = Console.ReadLine();
+        GameObject StlObject = StlAssetPostProcessor.CreateStlParent(path);
+        if (StlObject != null)
+        {
+            Console.WriteLine("Please enter if this object is fixed (true/false/Y/N");
+            string fixed_in = Console.ReadLine();
+            bool fix;
+            switch (fixed_in)
+            {
+                case "true":
+                case "Y":
+                    fix = true;
+                    break;
+                case "false":
+                case "N":
+                    fix = false;
+                    break;
+                default:
+                    fix = true;
+                    break;
+            }
+            if (fix)
+            {
+                StlObject.isStatic = true;
+            }
+            StlObject.SetActive(true);
+        }
+    }
+}
