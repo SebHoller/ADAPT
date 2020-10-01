dc7c640520102919b0445fab452a61ecf4fb5114
Sebastian <sebastian.holler@tum.de>
2020-10-01 09:08:48 +0200

    added tests for STL inport

---
+++ Unity/Assets/HumanMotionGeneration/TestSTL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSTL : MonoBehaviour
+{
+    AddObject addObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.addObject = gameObject.GetComponent<AddObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L) == true)
+        {
+            string path = "C:\\Projekte\\ADAPT\\Unity\\Assets\\HumanMotionGeneration\\10260_Workbench_max8_v1_iterations-2.stl";
+            this.addObject.IportSTL(path, true);
+        }
+        if (Input.GetKeyDown(KeyCode.K) == true)
+        {
+            string path = "C:\\Projekte\\ADAPT\\Unity\\Assets\\HumanMotionGeneration\\13604_Drill_Press_v1_L2.stl";
+            this.addObject.IportSTL(path, true);
+        }
+    }
+}
