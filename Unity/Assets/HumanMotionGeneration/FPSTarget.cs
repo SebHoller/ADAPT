9296a96bda4274ef7fa6f9ccb85111613fb151b3
Sebastian <sebastian.holler@tum.de>
2020-10-07 18:44:47 +0200

    added fps target

---
+++ Unity/Assets/HumanMotionGeneration/FPSTarget.cs
@@ -0,0 +1,44 @@
+﻿#region License
+/*
+* HumanMotionGeneration an expansion of Agent Development and Prototyping Testbed
+* https://github.com/Sebauer98/ADAPT
+* 
+* Copyright (C) 2020 Sebastian Holler
+*
+* This file is part of HumanMotionGeneration.
+* This project extends ADAPT.
+* 
+* ADAPT is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ADAPT is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with ADAPT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+using UnityEngine;
+
+public class FPSTarget : MonoBehaviour
+{
+    private int target = 60;
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.targetFrameRate != target)
+        {
+            Application.targetFrameRate = target;
+        }
+    }
+}
