083197ae84ef02291aea21daa5db6d4b00d61149
Sebastian <sebastian.holler@tum.de>
2020-10-10 21:30:15 +0200

    fixed output

--- Unity/Assets/HumanMotionGeneration/SkeletOutput.cs
+++ Unity/Assets/HumanMotionGeneration/SkeletOutput.cs
@@ -40,8 +40,7 @@ public class SkeletOutput : MonoBehaviour
         // csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         /* if(File.Exists("SkeletOutput.csv"))
         {
@@ -50,55 +49,54 @@ public class SkeletOutput : MonoBehaviour
             // csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
             // csv.Configuration.HasHeaderRecord = false;
         }*/
-        Character character = gameObject.GetComponent<Character>();
-        List<Output> list = new List<Output>
+        Character character = gameObject.GetComponent<Character>();        
+        Output output = new Output
         {
-            new Output
-            {
-                Time = DateTime.Now,
-                X = character.Body.Coordinator.hips.position.x,
-                Y = character.Body.Coordinator.hips.position.y,
-                Z = character.Body.Coordinator.hips.position.z,
-                RightShoulderX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").transform.position.x,
-                RightShoulderY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").transform.position.y,
-                RightShoulderZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").transform.position.z,
-                LeftShoulderX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").transform.position.x,
-                LeftShoulderY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LightShoulder").transform.position.y,
-                LeftShoulderZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").transform.position.z,
-                HeadX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("Neck").Find("Head").transform.position.x,
-                HeadY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("Neck").Find("Head").transform.position.y,
-                HeadZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("Neck").Find("Head").transform.position.z,
-                RightHandX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.x,
-                RightHandY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.y,
-                RightHandZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.z,
-                LeftHandX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.x,
-                LeftHandY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.y,
-                LeftHandZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.z,
-                RightFootX = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.x,
-                RightFootY = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.y,
-                RightFootZ = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.z,
-                LeftFootX = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.x,
-                LeftFootY = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.y,
-                LeftFootZ = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.z,
-                LeftArmX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").transform.position.x,
-                LeftArmY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").transform.position.y,
-                LeftArmZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("LeftShoulder").Find("LeftArm").transform.position.z,
-                LeftLegX = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.x,
-                LeftLegY = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.y,
-                LeftLegZ = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.z,
-                RightArmX = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").transform.position.x,
-                RightArmY = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").transform.position.y,
-                RightArmZ = character.Body.Coordinator.hips.Find("spine").Find("spine1").Find("spine2").Find("RightShoulder").Find("RightArm").transform.position.z,
-                RightLegX = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.x,
-                RightLegY = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.y,
-                RightLegZ = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.z
-            }
+            // Time = DateTime.Now,
+            Time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture),
+            X = character.Body.Coordinator.hips.position.x,
+            Y = character.Body.Coordinator.hips.position.y,
+            Z = character.Body.Coordinator.hips.position.z,
+            RightShoulderX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").transform.position.x,
+            RightShoulderY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").transform.position.y,
+            RightShoulderZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").transform.position.z,
+            LeftShoulderX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").transform.position.x,
+            LeftShoulderY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").transform.position.y,
+            LeftShoulderZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").transform.position.z,
+            HeadX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("Neck").Find("Head").transform.position.x,
+            HeadY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("Neck").Find("Head").transform.position.y,
+            HeadZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("Neck").Find("Head").transform.position.z,
+            RightHandX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.x,
+            RightHandY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.y,
+            RightHandZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").transform.position.z,
+            LeftHandX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.x,
+            LeftHandY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.y,
+            LeftHandZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").transform.position.z,
+            RightFootX = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.x,
+            RightFootY = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.y,
+            RightFootZ = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").Find("RightFoot").transform.position.z,
+            LeftFootX = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.x,
+            LeftFootY = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.y,
+            LeftFootZ = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").Find("LeftFoot").transform.position.z,
+            LeftArmX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").transform.position.x,
+            LeftArmY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").transform.position.y,
+            LeftArmZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("LeftShoulder").Find("LeftArm").transform.position.z,
+            LeftLegX = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.x,
+            LeftLegY = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.y,
+            LeftLegZ = character.Body.Coordinator.hips.Find("LeftUpLeg").Find("LeftLeg").transform.position.z,
+            RightArmX = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").transform.position.x,
+            RightArmY = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").transform.position.y,
+            RightArmZ = character.Body.Coordinator.hips.Find("Spine").Find("Spine1").Find("Spine2").Find("RightShoulder").Find("RightArm").transform.position.z,
+            RightLegX = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.x,
+            RightLegY = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.y,
+            RightLegZ = character.Body.Coordinator.hips.Find("RightUpLeg").Find("RightLeg").transform.position.z
         };
+
+        string text = "Time: " + output.Time + "; position: (" + output.X + "| " + output.Y + "| " + output.Z + "); right Shoulder: (" + output.RightShoulderX + "| " + output.RightShoulderY + "| " + output.RightShoulderZ + "); left Shoulder: (" + output.LeftShoulderX + "| " + output.LeftShoulderY + "| " + output.LeftShoulderZ + "); right Arm: (" + output.RightArmX + "| " + output.RightArmY + "| " + output.RightArmZ + "); left Arm: (" + output.LeftArmX + "| " + output.LeftArmY + "| " + output.LeftArmZ + "); right Hand: (" + output.RightHandX + "| " + output.RightHandY + "| " + output.RightHandZ + "); left Hand: (" + output.LeftHandX + "| " + output.LeftHandY + "| " + output.LeftHandZ + "); Head: (" + output.HeadX + "| " + output.HeadY + "| " + output.HeadZ + "); right Leg: (" + output.RightLegX + "| " + output.RightLegY + "| " + output.RightLegZ + "); left Leg:(" + output.LeftLegX + "| " + output.LeftLegY + "| " + output.LeftLegZ + "); right Foot:(" + output.RightFootX + "| " + output.RightFootY + "| " + output.RightFootZ + "); left Foot:( " + output.LeftFootX + "| " + output.LeftFootY + "| " + output.LeftFootZ + ");";
         using (StreamWriter file =
-            new StreamWriter(@"C:\\projects\\ADAPT\\Unity\\Assets\\HumanMotionGeneration\\SkeletOutput.txt", true))
+            new StreamWriter(@"C:\projects\ADAPT\Unity\Assets\HumanMotionGeneration\SkeletOutput.txt", true))
         {
-            file.WriteLine(list.ToString());
-            file.Close();
+            file.WriteLine(text);
         }
         // csv.WriteRecords(list);
     }
@@ -141,6 +139,7 @@ public class SkeletOutput : MonoBehaviour
         public float RightLegX { get; set; }
         public float RightLegY { get; set; }
         public float RightLegZ { get; set; }
-        public DateTime Time { get; set; }
+        //public DateTime Time { get; set; }
+        public string Time { get; set; }
     }
 }
